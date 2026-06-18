d14dc49f321807580b92534e33b034739a1253f9
Josh Mysore <joshmysore@college.harvard.edu>
2026-06-17 21:20:56 -0700

    Initial Commit

---
+++ Nett/NettApp.swift
@@ -0,0 +1,17 @@
+//
+//  NettApp.swift
+//  Nett
+//
+//  Created by Jōsh Mysoré on 6/17/26.
+//
+
+import SwiftUI
+
+@main
+struct NettApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
